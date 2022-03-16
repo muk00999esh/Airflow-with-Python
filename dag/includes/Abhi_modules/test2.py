129d97d6c899a4418d39f14466554aac69048089
muk00999esh <39761767+muk00999esh@users.noreply.github.com>
2022-03-17 05:00:19 +0530

    Update test2.py

--- dag/includes/Abhi_modules/test2.py
+++ dag/includes/Abhi_modules/test2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def see():
-    m = pd.read_csv('/usr/local/airflow/dags/Expenditure.csv')
+    m = pd.read_csv('./Expenditure.csv')
     #m = pd.read_csv('C:\dag\Expendture.csv')
     print(m.head())
     countt= m ['Category'].value_counts(sort=True, ascending=True).to_frame()
